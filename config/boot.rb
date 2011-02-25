973c53006d7b357ac5b9eaeadbc0957c44f2a783
Danil Pismenny <danil@orionet.ru>
2011-02-25 18:01:37 +0300

    Автосоздание базы для режима тест

--- config/boot.rb
+++ config/boot.rb
@@ -8,6 +8,8 @@ Bundler.require($environment)
 @config = YAML.load(File.read('config/config.yml'))[$environment]
 DataMapper.setup(:default, @config['database_url'])
 Dir["models/*.rb"].each{|f| require f}
-# DataMapper.finalize
+DataMapper.finalize
+
+DataMapper.auto_migrate! if $environment=='test'
 
 # DataMapper::Model.raise_on_save_failure = true  # globally across all models
